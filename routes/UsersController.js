fce33a84c385a8a15af869cf230f2114f33b2666
hanchen <hanchenjhc@gmail.com>
2019-09-09 20:54:53 -0500

    Use bcrypt to hash passwords
    [#167905111] (https://www.pivotaltracker.com/story/show/167905111)

--- routes/UsersController.js
+++ routes/UsersController.js
@@ -1,20 +1,24 @@
 import UsersService from "../user/UserService";
 import express from "express";
+import * as bcrypt from "bcrypt";
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 router.post('/createUser', (req, res, next) => {
-  const name = req.body.name;
-  const password = req.body.password;
-  const email = req.body.email;
-  UsersService.createUser(name, password, email);
-  res.status(201);
-  res.send();
+    const saltRounds = 14;
+    const name = req.body.name;
+    const password = req.body.password;
+    const email = req.body.email;
+    bcrypt.hash(password, saltRounds, (error, hash) => {
+        UsersService.createUser(name, hash, email);
+    });
+    res.status(201);
+    res.send();
 });
 
 export default router;
