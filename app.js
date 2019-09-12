e7070820c6a9da7aff3fb474b7cbe0f571f86949
hanchen <hanchenjhc@gmail.com>
2019-09-12 09:53:31 -0500

    Load database connection from cf environment
    [#167905111] (https://www.pivotaltracker.com/story/show/167905111)

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -22,21 +21,4 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.use('/users', usersRouter);
 
-// app.use('/', indexRouter);
-//
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     next(createError(404));
-// });
-//
-// // error handler
-// app.use(function (err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//     // render the error page
-//     res.status(err.status || 500);
-//     // res.render('error');
-// });
 export default app;
--- database.js
+++ database.js
@@ -1,11 +1,16 @@
+const cfenv = require('cfenv');
+
+const appEnv = cfenv.getAppEnv();
+console.log("got application environment: ", appEnv);
+const dbConnectionString = appEnv.elephantsql ? appEnv.elephantsql[0].credentials.uri : 'postgres://enso-street:password@localhost:5432/enso-street';
+
 const initOptions = {
     // initialization options;
 };
 
 const pgp = require('pg-promise')(initOptions);
 
-const cn = 'postgres://enso-street:password@localhost:5432/enso-street';
-const db = pgp(cn);
+const db = pgp(dbConnectionString);
 
 module.exports = {
     pgp, db
