bf73b051fe9d5c2a0bc66d440d28ab2b966950c5
hanchen <hanchenjhc@gmail.com>
2019-09-23 20:42:08 -0500

    extract session options to file

--- app.js
+++ app.js
@@ -12,42 +12,20 @@ import cors from "cors";
 import https from "https";
 import fs from "fs";
 import session from "express-session";
-import redis from "redis";
 import passport from "passport";
 import setupPassport from "./setupPassport";
+import sessionOptions from "./sessionOptions";
 
 dotenv.config();
 const uiDomain = process.env.uiBaseUrl;
-const redisHost = process.env.redisHost;
-const redisPassword = process.env.redisPassword;
-const redisPort = process.env.redisPort;
-
-const redisStore = require('connect-redis')(session);
-const redisClient = redis.createClient({
-        port: redisPort,
-        host: redisHost,
-        password: redisPassword
-    }
-);
 
 setupPassport();
 
 const app = express();
 app.use(cors({origin: uiDomain, optionsSuccessStatus: 200, credentials: true}));
 
-let cookieExpirationInMils = 1000 * 60 * 30;
 app.set('trust proxy', 1);
-app.use(session({
-    cookie: {
-        secure: true,
-        httpOnly: true,
-        maxAge: cookieExpirationInMils
-    },
-    secret: process.env.sessionSecret,
-    store: new redisStore({host: redisHost, password: redisPassword, port: redisPort, client: redisClient, ttl: 260}),
-    saveUninitialized: false,
-    resave: false
-}));
+app.use(session(sessionOptions));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
