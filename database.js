a513184f80285b465bbc510c47852a30ccb959e3
hanchen <hanchenjhc@gmail.com>
2019-12-08 21:33:27 -0600

    remove databaseUri env var and use DATABASE_URL env var instead

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import pgp from 'pg-promise';
+
 dotenv.config();
 
-const dbConnectionString = process.env.databaseUri;
+const dbConnectionString = process.env.DATABASE_URL;
 const initOptions = {};
 
 const db = pgp(initOptions)(dbConnectionString);
