41324db228109b09d6409145e80eb580f522a04e
hanchen <hanchenjhc@gmail.com>
2019-12-29 22:10:42 -0600

    Attempt to add geography column for item
    [#170447954] (https://www.pivotaltracker.com/story/show/170447954)

---
+++ migrations/20191230032520-add-lat-long-for-items.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+    db.runSql(`CREATE EXTENSION IF NOT EXISTS "Postgis";`);
+
+    db.runSql(
+            `ALTER TABLE public.item
+                ADD COLUMN geo_location GEOGRAPHY;`, callback
+    )
+};
+
+exports.down = function (db) {
+    return null;
+};
+
+exports._meta = {
+    "version": 1
+};
