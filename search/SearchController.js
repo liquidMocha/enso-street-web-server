bc0e87f68d92d022a51a0e0561013331b2835800
hanchen <hanchenjhc@gmail.com>
2020-01-01 22:47:59 -0600

    Search endpoint can search by address string instead of coordinates

--- search/SearchController.js
+++ search/SearchController.js
@@ -1,19 +1,40 @@
 import express from "express";
 import ItemRepository from "../item/ItemRepository";
+import HereApiClient from "../location/HereApiClient";
 
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
     const searchTerm = req.body.searchTerm;
-    const {latitude, longitude} = req.body.coordinates;
+    const coordinates = req.body.coordinates;
+    if (!coordinates) {
+        const address = req.body.address;
 
-    ItemRepository.getItemsInRangeFrom({latitude, longitude}, 5)
-        .then(result => {
-            res.status(200).json(result);
-        })
-        .catch(error => {
-            console.error(`Error when searching: ${error}`);
-        });
+        HereApiClient.geocode(address)
+            .then((coordinates) => {
+                ItemRepository.getItemsInRangeFrom({
+                    latitude: coordinates.latitude,
+                    longitude: coordinates.longitude
+                }, process.env.SEARCH_RANGE_IN_MILES)
+                    .then(result => {
+                        res.status(200).json(result);
+                    })
+                    .catch(error => {
+                        console.error(`Error when searching: ${error}`);
+                    });
+            })
+    } else {
+        ItemRepository.getItemsInRangeFrom({
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        }, process.env.SEARCH_RANGE_IN_MILES)
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch(error => {
+                console.error(`Error when searching: ${error}`);
+            });
+    }
 });
 
 export default router;
