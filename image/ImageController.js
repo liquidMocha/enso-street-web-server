85ceb053bc8e77fe590dc2eeaaab7462507df90c
hanchen <hanchenjhc@gmail.com>
2020-01-12 14:13:12 -0600

    Send image url instead of a key when getting signed S3 request
    [#170507285] (https://www.pivotaltracker.com/story/show/170507285)

--- image/ImageController.js
+++ image/ImageController.js
@@ -8,7 +8,10 @@ router.get('/signedS3Request', async (req, res, next) => {
     const key = uuidv4();
     const signedRequest = await ImageRepository.getSignedS3Request(key);
 
-    res.status(200).json({key: key, uploadRequest: signedRequest});
+    res.status(200).json({
+        uploadRequest: signedRequest,
+        imageUrl: `https://${process.env.Bucket}.s3.amazonaws.com/${key}`
+    });
 });
 
 export default router;
