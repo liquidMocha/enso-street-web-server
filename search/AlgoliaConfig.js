a69ea1209474b0ca62594002c7834474d2426820
hanchen <hanchenjhc@gmail.com>
2020-01-15 22:13:04 -0600

    Add dummy algolia config
    [#170387626] (https://www.pivotaltracker.com/story/show/170387626)

--- search/AlgoliaConfig.js
+++ search/AlgoliaConfig.js
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch';
 
-const client = algoliasearch(process.env.ALGOLIASEARCH_APPLICATION_ID, process.env.ALGOLIASEARCH_API_KEY);
-const searchIndex = client.initIndex(process.env.ALGOLIA_ITEM_INDEX);
+const client = algoliasearch(process.env.ALGOLIASEARCH_APPLICATION_ID || 'dummy', process.env.ALGOLIASEARCH_API_KEY || 'dummy');
+const searchIndex = client.initIndex(process.env.ALGOLIA_ITEM_INDEX || 'dummy');
 
 export default searchIndex;
