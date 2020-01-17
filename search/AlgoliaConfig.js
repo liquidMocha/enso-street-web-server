efb3cf366d9c3ebb3e477071223c376f67e07c62
hanchen <hanchenjhc@gmail.com>
2020-01-17 12:39:41 -0600

    Add searchable attributes to algolia config
    [#170387626] (https://www.pivotaltracker.com/story/show/170387626)

--- search/AlgoliaConfig.js
+++ search/AlgoliaConfig.js
@@ -3,4 +3,12 @@ import algoliasearch from 'algoliasearch';
 const client = algoliasearch(process.env.ALGOLIASEARCH_APPLICATION_ID || 'dummy', process.env.ALGOLIASEARCH_API_KEY || 'dummy');
 const searchIndex = client.initIndex(process.env.ALGOLIA_ITEM_INDEX || 'dummy');
 
+searchIndex.setSettings({
+    searchableAttributes: [
+        'unordered(title)',
+        'unordered(categories)',
+        'description'
+    ]
+});
+
 export default searchIndex;
