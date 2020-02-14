818cdfb9aa35fe73f0f2ae484aa8a6c5e8857c78
hanchen <hanchenjhc@gmail.com>
2020-02-14 17:51:42 -0600

    Add pg monitor

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import pgp from 'pg-promise';
+import monitor from 'pg-monitor';
 
 dotenv.config();
 
 const dbConnectionString = process.env.DATABASE_URL;
 const initOptions = {};
+monitor.attach(initOptions);
 
 const db = pgp(initOptions)(dbConnectionString);
 
