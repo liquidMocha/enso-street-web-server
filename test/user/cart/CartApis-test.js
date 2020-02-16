4796145eede994307e72ade6a3227e1956cbe828
hanchen <hanchenjhc@gmail.com>
2020-02-16 13:30:56 -0600

    Add endpoint to get cart of items
    [#171086818] (https://www.pivotaltracker.com/story/show/171086818)

--- test/user/cart/CartApis-test.js
+++ test/user/cart/CartApis-test.js
@@ -4,18 +4,21 @@ import app from "../../../app";
 import {getAuthenticatedApp} from "../../TestHelper";
 import * as CartRepository from "../../../user/cart/CartRepository";
 import {assert} from "chai";
+import ItemRepository from "../../../item/ItemRepository";
+import {ItemDAO} from "../../../item/ItemDAO";
 
 describe('cart', () => {
     const loggedInUserEmail = "abc@enso.com";
     const authenticatedApp = getAuthenticatedApp(loggedInUserEmail);
     let getCartStub;
     let addToCartStub;
-    const cart = [{itemId: "abc123"}];
+    let getItemByIdStub;
 
     beforeEach(() => {
         sinon.resetHistory();
-        getCartStub = sinon.stub(CartRepository, 'getCart').resolves(cart);
-        addToCartStub = sinon.stub(CartRepository, 'addItemForUser')
+        getCartStub = sinon.stub(CartRepository, 'getCart');
+        addToCartStub = sinon.stub(CartRepository, 'addItemForUser');
+        getItemByIdStub = sinon.stub(ItemRepository, 'getItemById');
     });
 
     afterEach(() => {
@@ -32,10 +35,72 @@ describe('cart', () => {
         });
 
         it('should get cart for logged in user', (done) => {
+            const itemId1 = "abc123";
+            const itemTitle1 = "small cat";
+            const ownerEmail1 = "abc@enso.com";
+            const rentalDailyPrice1 = 12.23;
+            const imageUrl1 = "abc-123.jpg";
+
+            const itemId2 = "cde456";
+            const itemTitle2 = "big dog";
+            const ownerEmail2 = "snoopy@enso.com";
+            const rentalDailyPrice2 = 12.23;
+            const imageUrl2 = "abc-613.jpg";
+
+            getCartStub.resolves([{itemId: itemId1}, {itemId: itemId2}]);
+
+            getItemByIdStub.onCall(0).resolves(new ItemDAO({
+                id: itemId1,
+                title: itemTitle1,
+                ownerEmail: ownerEmail1,
+                deposit: 123,
+                rentalDailyPrice: rentalDailyPrice1,
+                deliveryAdditional: 2,
+                deliveryStarting: 10,
+                condition: "Like new",
+                description: "something something something vicky",
+                imageUrl: imageUrl1
+            }));
+            getItemByIdStub.onCall(1).resolves(new ItemDAO({
+                id: itemId2,
+                title: itemTitle2,
+                ownerEmail: ownerEmail2,
+                deposit: 123,
+                rentalDailyPrice: rentalDailyPrice2,
+                deliveryAdditional: 2,
+                deliveryStarting: 10,
+                condition: "Like new",
+                description: "something something something vicky",
+                imageUrl: imageUrl2
+            }));
+
+            const expectedOwnerBatch1 = [
+                {
+                    title: itemTitle1,
+                    id: itemId1,
+                    rentalDailyPrice: rentalDailyPrice1,
+                    imageUrl: imageUrl1
+                }
+            ];
+
+            const expectedOwnerBatch2 = [
+                {
+                    title: itemTitle2,
+                    id: itemId2,
+                    rentalDailyPrice: rentalDailyPrice2,
+                    imageUrl: imageUrl2
+                }
+            ];
+
             request(authenticatedApp)
                 .get('/api/cart')
                 .expect(200, (error, response) => {
-                    assert.deepEqual(response.body, cart);
+                    sinon.assert.calledWith(getItemByIdStub, itemId1);
+                    sinon.assert.calledWith(getItemByIdStub, itemId2);
+                    assert.deepEqual(response.body, {
+                        [ownerEmail1]: expectedOwnerBatch1,
+                        [ownerEmail2]: expectedOwnerBatch2
+                    });
                     done(error);
                 })
         })
--- user/cart/CartController.js
+++ user/cart/CartController.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {addItemForUser, getCart} from "./CartRepository";
+import ItemRepository from "../../item/ItemRepository";
+import _ from "lodash";
 
 const router = express.Router();
 
@@ -7,10 +9,24 @@ router.get('/', async (req, res, next) => {
     const userEmail = req.session.email;
     if (userEmail) {
         try {
-            const cart = await getCart(userEmail);
-            res.status(200).json(cart);
+            const itemIds = await getCart(userEmail);
+            const itemDAOs = await Promise.all(itemIds.map(item => {
+                return ItemRepository.getItemById(item.itemId)
+            }));
+            const ownerBatches = _.groupBy(itemDAOs, 'ownerEmail');
+            const cartDTO = _.mapValues(ownerBatches, (daos) => {
+                return daos.map(dao => {
+                    return {
+                        id: dao.id,
+                        title: dao.title,
+                        rentalDailyPrice: dao.rentalDailyPrice,
+                        imageUrl: dao.imageUrl
+                    }
+                });
+            });
+            res.status(200).json(cartDTO);
         } catch (e) {
-            console.error(`Error when retrieving cart for user ${userEmail}: ${e}`)
+            console.error(`Error when retrieving cart for user ${userEmail}: ${e}`);
             res.status(500).send();
         }
     } else {
