c50320b511e0d8d01939a2e244508f4729b9bef3
hanchen <hanchenjhc@gmail.com>
2021-03-15 21:45:49 -0500

    set ssl to true on application pg connections

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,8 @@ const dbConnectionString: string = process.env.DATABASE_URL!;
 const initOptions = {};
 
 const pgp = pgPromise(initOptions);
+pgp.pg.defaults.ssl = true;
+
 const db = pgp(dbConnectionString);
 
 export default db;
